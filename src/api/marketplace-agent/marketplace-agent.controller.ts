e3fe751238f944c64f6883fee21e832c5f658b9b
resister-boy <hassanpumped17@gmail.com>
2025-04-05 23:52:03 +0900

    fix: typo

--- src/api/marketplace-agent/marketplace-agent.controller.ts
+++ src/api/marketplace-agent/marketplace-agent.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { MarketplaceAgentService } from './marketplace-agent.service';
 import { ApiTags } from '@nestjs/swagger';
 import { IsSavedDto, SaveMarketplaceAgentDto } from './marketplace-agent.dto';
@@ -6,7 +14,9 @@ import { IsSavedDto, SaveMarketplaceAgentDto } from './marketplace-agent.dto';
 @ApiTags('Marketplace Agent')
 @Controller('/api/marketplace-agents')
 export class MarketplaceAgentController {
-  constructor(private readonly marketplaceAgentService: MarketplaceAgentService) {}
+  constructor(
+    private readonly marketplaceAgentService: MarketplaceAgentService,
+  ) {}
 
   @Get()
   async get_marketplace_agents() {
@@ -19,12 +29,15 @@ export class MarketplaceAgentController {
   }
 
   @Post(':id/save')
-  async save_marketplace_agent(@Param('id') id: string, @Body() data: SaveMarketplaceAgentDto) {
+  async save_marketplace_agent(
+    @Param('id') id: string,
+    @Body() data: SaveMarketplaceAgentDto,
+  ) {
     return await this.marketplaceAgentService.save_marketplace_agent(id, data);
   }
 
-  @Post(':id/is-saved ')
+  @Post(':id/is-saved')
   async is_saved(@Param('id') id: string, @Body() data: IsSavedDto) {
     return await this.marketplaceAgentService.is_saved(id, data);
   }
-} 
+}
